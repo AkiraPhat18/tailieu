5e0e5d4d83cd09268bfcdecea0d3bc8a6c0057f1
AkiraPhat18 <phatnguyen19111997@gmail.com>
2022-02-22 20:16:45 +0700

    first commit

---
+++ on lap trinh C thi samsung/Untitled1.cpp
@@ -0,0 +1,11 @@
+//Code new
+#include<stdio.h>
+//hàm main
+int main() {
+	//printf("Hello");
+	int a , b , sum ;//khai bao bien
+	scanf("%d%d", &a , &b );//%d giá tri thu nhat, hai so a va b la so nguyen
+	sum = a+b ;
+	printf("%d + %d = %d",a , b , sum);
+	return 0;
+}
