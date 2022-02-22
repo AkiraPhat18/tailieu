2d52188a094a0f2cdf628b734c940f1c5a73b9bf
AkiraPhat18 <phatnguyen19111997@gmail.com>
2022-02-22 21:14:59 +0700

    new commit

--- on lap trinh C thi samsung/Untitled1.cpp
+++ on lap trinh C thi samsung/Untitled1.cpp
@@ -1,9 +1,14 @@
 //Code new
 #include<stdio.h>
+
+/*int Pi ;
+double lai suat;*/ // bien toan cuc khai bao cac hang so 
+
 //hàm main
 int main() {
 	//printf("Hello");
-	int a , b , sum ;//khai bao bien
+	int a , b , sum ;//khai bao bien 
+	// day la bien cuc bo 
 	scanf("%d%d", &a , &b );//%d giá tri thu nhat, hai so a va b la so nguyen
 	sum = a+b ;
 	printf("%d + %d = %d",a , b , sum);
