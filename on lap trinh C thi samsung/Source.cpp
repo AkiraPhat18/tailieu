91451f916ce352e59d9283bd42e6086c3fe5cc06
AkiraPhat18 <phatnguyen19111997@gmail.com>
2022-02-24 20:57:01 +0700

    new commit

---
+++ on lap trinh C thi samsung/Source.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+
+using namespace std;
+
+int main (){
+	//freopen("input.txt", "r", stdin);
+	
+	int T;//tong so test case
+	cin >> T;
+	for(int tc =1;tc <= T ; tc++){
+		int n , x , y;
+		cin >> n >> x >> y;
+		for(int i=1; i < n; i++){
+			if( i % x == 0 & i % y != 0){
+				cout <<  i << " ";
+			}
+			
+		}
+	}
+	cout << endl;
+	return 0;
+}
